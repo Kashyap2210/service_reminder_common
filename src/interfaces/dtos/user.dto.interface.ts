a18a7751ade5fba7a65e4915949a446821bb81bc
Kashyap Solanki <kash.cdac@gmail.com>
2026-05-25 23:41:40 +0530

    SR-48-status-add-in-IUserCreateDto

--- src/interfaces/dtos/user.dto.interface.ts
+++ src/interfaces/dtos/user.dto.interface.ts
@@ -10,7 +10,9 @@ export type IUserDtoExclude = "status" | "role";
 export interface IUserCreateDto extends Omit<
   IEntityCreateDto<EntityType<EntityList.USER>>,
   IUserDtoExclude
-> {}
+> {
+  status?: UserStatus;
+}
 
 export interface IUserUpdateDto extends Omit<
   IEntityUpdateDto<EntityType<EntityList.USER>>,
